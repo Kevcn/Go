2411ef4e03116da8456adb3ffa587ea18e7c58e2
KevinLi <kevin.li@sharpgaming.com>
2021-02-14 14:13:52 +0000

    handlers in its own package, adding graceful shutdown to the app

--- service/Main.go
+++ service/Main.go
@@ -1,38 +1,69 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/kevcn/service/handlers"
 )
 
 func main() {
 
-	// register a path to the server MUX (Multiplexer that select which handler handles a HTTP request)
-	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-		log.Println("Hello World")
+	/// Lesson 1
 
-		// capture the data and potential error from the request body
-		d, error := ioutil.ReadAll(r.Body)
+	// // register a path to the server MUX (Multiplexer that select which handler handles a HTTP request)
+	// http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
 
-		if error != nil {
-			// Allows us to send respond status code
-			http.Error(rw, "Ooops", http.StatusBadRequest)
-			return
+	// })
 
-		}
-
-		log.Printf("Data %s\n", d)
-
-		fmt.Fprintf(rw, "Hello %s", d)
-	})
-
-	// Just another end point
-	http.HandleFunc("/goodBye", func(http.ResponseWriter, *http.Request) {
-		log.Println("Good bye")
-	})
+	// // Just another end point
+	// http.HandleFunc("/goodBye", func(http.ResponseWriter, *http.Request) {
+	// 	log.Println("Good bye")
+	// })
 
 	// Start and listen the server on a port
-	http.ListenAndServe(":8080", nil)
+	// http.ListenAndServe(":8080", nil)
+
+	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+	hh := handlers.NewHello(l)
+
+	// Create our own server multiplexer
+	sm := http.NewServeMux()
+	sm.Handle("/", hh)
+
+	// Create a server with timeouts to prevent attacks
+	s := &http.Server{
+		Addr:         ":9090",
+		Handler:      sm,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+
+	// place on a go func so that it doesn't stop listening on the port
+	go func() {
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Fatal(err)
+		}
+	}()
+
+	// making a channel that passes the type Signal from os package
+	sigChan := make(chan os.Signal)
+
+	// broadcast an os command into the sigChan channel
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+
+	sig := <-sigChan
+	l.Println("Received terminate, graceful shutdown", sig)
+
+	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
+
+	// gracefully shut down
+	s.Shutdown(tc)
 }
---
+++ service/handlers/hello.go
@@ -0,0 +1,33 @@
+package handlers
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+type Hello struct {
+	l *log.Logger
+}
+
+func NewHello(l *log.Logger) *Hello {
+	return &Hello{l}
+}
+
+// This is an extension on the hello struct? 	// struct is referenced with a struct
+func (h *Hello) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	h.l.Println("Hello World")
+
+	// capture the data and potential error from the request body
+	d, error := ioutil.ReadAll(r.Body)
+
+	if error != nil {
+		// Allows us to send respond status code
+		http.Error(rw, "Ooops", http.StatusBadRequest)
+		return
+
+	}
+
+	fmt.Fprintf(rw, "Hello %s", d)
+}
